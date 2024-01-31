b4bcad3206817d097f9c205217d4f997f7b1d61c
Abhilash Bande <abhilash.bande@gmail.com>
2024-01-31 20:50:40 +0530

    session 31 jan : loops

---
+++ BasicsOfJava/src/com/core/loops/Loops.java
@@ -0,0 +1,67 @@
+package com.core.loops;
+
+import java.util.Scanner;
+
+public class Loops {
+
+	public static void main(String[] args) {
+
+		int number = 12;
+		// for
+
+		// for (initialization; condition; increment/decrement)
+//		for (int i = 1; i <= 10; i++) {
+//			System.out.println(number * i);
+//		}
+
+		// while
+//		int i = 1;
+//		while(i <= 10) {
+//			System.out.println(number * i);
+//			i++;
+//		}
+
+		// do-while
+
+//		int userNumber = 0;
+//		Scanner sc = new Scanner(System.in);
+//		
+//		do {
+//			System.out.print("Enter a number (0 to exit): " );
+//			userNumber = sc.nextInt();
+//			System.out.println("Number = " + userNumber);
+//		} while (userNumber != 0);
+//		
+//		sc.close();
+
+		// break
+//		for (int i = 1; i <= 10; i++) {
+//			if (i == 5) {
+//				break;
+//			}
+//			System.out.println(i);
+//		}
+
+		// continue
+//		for (int i = 1; i <= 10; i++) {
+//			if (i == 5) {
+//				continue;
+//			}
+//			System.out.println(i);
+//		}
+
+//		for (int i = 1; i <= 10; i++) {
+//			if (i == 5) {
+//				System.exit(0);
+//				// 0 --> successful termination
+//				// 1 --> Unsuccessful termination
+//				// -1 --> Unsuccessful termination with an exception
+//				
+//			}
+//			System.out.println(i);
+//		}
+//
+//		System.out.println("Exit");
+	}
+
+}
